d84d19d60bc50151f719cc3220e12e7eb6f2b3dd
Juan Monroy-Nieto <jmonroynieto@outlook.com>
2024-05-03 21:11:56 -0500

    library working TODO fix rendering

---
+++ support.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// STYLING
+
+type keyMap struct {
+	up       key.Binding
+	down     key.Binding
+	quitKeys key.Binding
+}
+
+type styles struct {
+	Base,
+	HeaderText,
+	Documentation,
+	StatusHeader,
+	Highlight,
+	ErrorHeaderText,
+	Help lipgloss.Style
+}
+
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.Copy().BorderStyle(b)
+	}()
+)
+
+var (
+	red    = lipgloss.AdaptiveColor{Light: "#FE5F86", Dark: "#FE5F86"}
+	indigo = lipgloss.AdaptiveColor{Light: "#5A56E0", Dark: "#7571F9"}
+	green  = lipgloss.AdaptiveColor{Light: "#02BA84", Dark: "#02BF87"}
+)
+
+func newStyles(lg *lipgloss.Renderer) *styles {
+	s := styles{}
+	s.Base = lg.NewStyle().
+		Padding(1, 4, 0, 1)
+	s.HeaderText = lg.NewStyle().
+		Foreground(indigo).
+		Bold(true).
+		Padding(0, 1, 0, 2)
+	s.Documentation = lg.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(indigo).
+		PaddingLeft(1).
+		MarginTop(1)
+	s.StatusHeader = lg.NewStyle().
+		Foreground(green).
+		Bold(true)
+	s.Highlight = lg.NewStyle().
+		Foreground(lipgloss.Color("212"))
+	s.ErrorHeaderText = s.HeaderText.Copy().
+		Foreground(red)
+	s.Help = lg.NewStyle().
+		Foreground(lipgloss.Color("240"))
+	return &s
+}
+
+var Km = keyMap{
+	up: key.NewBinding(
+		key.WithKeys("k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	down: key.NewBinding(
+		key.WithKeys("j", ""),
+		key.WithHelp("↓/j", "move down"),
+	),
+	quitKeys: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("", "press q to quit"),
+	),
+}
+
+// LAYOUT
+
+func (m model) headerView() string {
+	title := titleStyle.Render("Mr. Pager")
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+}
+
+func (m model) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func (m model) errorView() string {
+	var s string
+	for _, err := range m.form.Errors() {
+		s += err.Error()
+	}
+	return s
+}
+
+func (m model) appBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.HeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(indigo),
+	)
+}
+
+func (m model) appErrorBoundaryView(text string) string {
+	return lipgloss.PlaceHorizontal(
+		m.width,
+		lipgloss.Left,
+		m.styles.ErrorHeaderText.Render(text),
+		lipgloss.WithWhitespaceChars("/"),
+		lipgloss.WithWhitespaceForeground(red),
+	)
+}
---
+++ tui.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/pydpll/errorutils"
+)
+
+const maxWidth = 80
+
+type errMsg error
+
+type model struct {
+	form          *huh.Form
+	viewport      viewport.Model
+	vReady        bool
+	documentation *string
+	err           error
+	width         int
+	lg            *lipgloss.Renderer
+	styles        *styles
+}
+
+func initialModel(f *huh.Form, d *string) model {
+	m := model{
+		form: f.
+			WithWidth(45).
+			WithShowHelp(false).
+			WithShowErrors(false),
+		documentation: d,
+		width:         maxWidth,
+		lg:            lipgloss.DefaultRenderer(),
+	}
+	m.styles = newStyles(m.lg)
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	one := m.form.Init()
+	two := m.viewport.Init()
+	return tea.Sequence(one, two)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		if key.Matches(msg, Km.quitKeys) {
+			return m, tea.Quit
+		} else if key.Matches(msg, Km.up) || key.Matches(msg, Km.down) {
+			var cmd tea.Cmd
+			m.viewport, cmd = m.viewport.Update(msg)
+			return m, cmd
+		}
+		aform, cmd := m.form.Update(msg)
+		if f, ok := aform.(*huh.Form); ok {
+			m.form = f
+		}
+		return m, cmd
+	case errMsg:
+		m.err = msg
+		return m, nil
+	case tea.WindowSizeMsg:
+		m.width = min(msg.Width, maxWidth) - m.styles.Base.GetHorizontalFrameSize()
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight
+		if !m.vReady {
+			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight-8)
+			m.viewport.YPosition = headerHeight
+			m.viewport.SetContent(*m.documentation)
+			m.vReady = true
+			m.viewport.YPosition = headerHeight + 1
+		} else {
+			m.viewport.Width = msg.Width
+			m.viewport.Height = msg.Height - verticalMarginHeight
+		}
+
+	default:
+		frm, cmd := m.form.Update(msg)
+		m.form = frm.(*huh.Form)
+		if cmd != nil {
+			cmds = append(cmds, cmd)
+		}
+	}
+
+	if m.form.State == huh.StateCompleted {
+		var cmd tea.Cmd
+		m.viewport, cmd = m.viewport.Update(tea.QuitMsg{})
+		cmds = append(cmds, tea.Quit, cmd)
+	}
+	return m, tea.Sequence(cmds...)
+}
+
+func (m model) View() string {
+	v := strings.TrimSuffix(m.form.View(), "\n\n")
+	form := m.lg.NewStyle().Margin(1, 0).Render(v)
+
+	const docWidth = 28
+	docMarginLeft := m.width - docWidth - lipgloss.Width(form) - m.styles.Documentation.GetMarginRight()
+	m.viewport.Style.MarginLeft(docMarginLeft)
+	m.viewport.Style.Height(lipgloss.Height(form))
+	m.viewport.Style.Width(docWidth)
+
+	if !m.vReady {
+		return "\n  Initializing..."
+	}
+	docPanel := fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+	errors := m.form.Errors()
+	header := m.appBoundaryView("Options and notes")
+	if len(errors) > 0 {
+		header = m.appErrorBoundaryView(m.errorView())
+	}
+	body := lipgloss.JoinHorizontal(lipgloss.Top, form, docPanel)
+
+	footer := m.appBoundaryView(m.form.Help().ShortHelpView(m.form.KeyBinds()))
+	if len(errors) > 0 {
+		footer = m.appErrorBoundaryView("")
+	}
+
+	return m.styles.Base.Render(header + "\n" + body + "\n\n" + footer)
+}
+
+func New(f *huh.Form, d *string) *tea.Program {
+	return tea.NewProgram(initialModel(f, d))
+}
